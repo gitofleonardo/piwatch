51e70b73acb12b1e912b862489b23b8a82ef1aba
huangchengxi <971840889@qq.com>
2020-12-25 22:39:16 +0800

    1

---
+++ src/main/kotlin/cn/huangchengxi/piwatch/PiwatchApplication.kt
@@ -0,0 +1,11 @@
+package cn.huangchengxi.piwatch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PiwatchApplication
+
+fun main(args: Array<String>) {
+    runApplication<PiwatchApplication>(*args)
+}
---
+++ src/test/kotlin/cn/huangchengxi/piwatch/PiwatchApplicationTests.kt
@@ -0,0 +1,13 @@
+package cn.huangchengxi.piwatch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PiwatchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
